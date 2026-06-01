404579e17e03da6fba187419bdc79c67dd5046b5
Georges Palauqui <georges@foundationdevices.com>
2026-06-01 13:45:05 +0200

    update to 215.0.1-beta001

--- default.nix
+++ default.nix
@@ -45,11 +45,11 @@
 
 stdenv.mkDerivation rec {
   pname = "roam";
-  version = "212.0.0-beta001";
+  version = "215.0.1-beta001";
 
   src = fetchurl {
     url = "https://download.ro.am/Roam/8a86d88cfc9da3551063102e9a4e2a83/linux/debian/binary/${version}-roam_${version}_amd64.deb";
-    sha256 = "0fiw2cmvwylsv6bb42cc1c0yr8sjnky842z196wanj8r8hywxn8s";
+    sha256 = "0hvigdcpd7ljs19kx0v8i568yskmi9mbk0hcsrbxl71ssx739fbr";
   };
 
   nativeBuildInputs = [
